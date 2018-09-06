f382ba109a686493681e3412c0044db475636c29
fuckyou211 <527644254@qq.com>
2018-09-06 08:32:15 +0800

    修改了mybatis generator的配置

--- src/main/java/com/shoes101/mapper/DemoMapper.java
+++ src/main/java/com/shoes101/mapper/DemoMapper.java
@@ -2,7 +2,7 @@ package com.shoes101.mapper;
 
 import com.shoes101.pojo.Demo;
 import java.util.List;
-//通过XML进行配置 mapper.xml
+
 public interface DemoMapper {
     int deleteByPrimaryKey(Integer did);
 
