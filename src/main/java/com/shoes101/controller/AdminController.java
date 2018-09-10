2d38e84c2fd269057c47f16c438c2e370df8013f
fuckyou211 <527644254@qq.com>
2018-09-10 17:34:06 +0800

    将AdminController中的登录功能的代码移动到Service中

--- src/main/java/com/shoes101/controller/AdminController.java
+++ src/main/java/com/shoes101/controller/AdminController.java
@@ -1,11 +1,9 @@
 package com.shoes101.controller;
 
 import com.alibaba.fastjson.JSON;
-import com.shoes101.pojo.Admin;
 import com.shoes101.pojo.Property;
 import com.shoes101.service.AdminService;
 import com.shoes101.service.PropertyService;
-import com.shoes101.util.MD5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -100,40 +98,7 @@ public class AdminController {
     public Object doLogin(HttpServletResponse response, @RequestParam(value="adminName") String adminName,
                           @RequestParam(value="password") String password)
     {
-        //String data = "";
-        //System.out.println("aaaa");
-        //MD5Util md5Util = new MD5Util();
-//        System.out.println("pass");
-//        System.out.println(password);
-
-        //首次加密
-        //String secondPass = MD5Util.inputPassToFormPass(password);
-
-        //System.out.println(secondPass);
-        System.out.println("Good afternoon");
-        //二次加密
-        String endPassword = MD5Util.formPassToDBPass(password,MD5Util.getSalt());
-
-        //System.out.println(endPassword);
-        //加密的密码在数据库找对应
-        Admin admin = adminservice.findAdmin(adminName);
-        //System.out.println(admin.getPassword());
-        //System.out.println(admin.getAdminName());
-        if(admin == null)
-        {
-            //data = "error";
-            //System.out.println("1");
-            return "0";
-        }
-        else if (admin.getPassword().equals(endPassword))
-        {
-            //data = "yes";
-            //System.out.println("2");
-            return "1";
-        }
-       // data = "error";
-        //System.out.println("3");
-        return "0";
+        return adminservice.findAdmin(adminName,password);
     }
 
     //分类管理
