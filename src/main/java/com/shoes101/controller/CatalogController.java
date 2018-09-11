a1cb3e24f2e7d562db6194867a824a443e1ae092
DlSco <734808034@qq.com>
2018-09-11 16:21:20 +0800

    添加获得父分类节点的controller mapping

--- src/main/java/com/shoes101/controller/CatalogController.java
+++ src/main/java/com/shoes101/controller/CatalogController.java
@@ -72,13 +72,24 @@ public class CatalogController {
     }
 
     /**
-     * 点击上方分类处理
+     * 点击上方分类路径的处理
      */
 
     @RequestMapping("/respondCatalogPath")
     @ResponseBody
     public Result<String> respondCatalogPath(Integer catalogId){
         List<Shoescatalog> list = catalogService.getChildList(catalogId);
+        String catalogLost = JSON.toJSONString(list);
+        return Result.success(catalogLost);
+    }
+
+    /**
+     * 获取根分类
+     */
+    @RequestMapping("/getParentCatalog")
+    @ResponseBody
+    public Result<String> getParentCatalog(){
+        List<Shoescatalog> list = catalogService.getParentCatalog(0);
         String catalogLost = JSON.toJSONString(list);
         return Result.success(catalogLost);
     }
