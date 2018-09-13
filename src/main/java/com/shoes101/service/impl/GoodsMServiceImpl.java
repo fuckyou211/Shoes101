dabd461cd08806c8447d7c804c248d2b45a3c3c4
Tbestf <1109102506@qq.com>
2018-09-13 09:23:52 +0800

    VCS更新

---
+++ src/main/java/com/shoes101/service/impl/GoodsMServiceImpl.java
@@ -0,0 +1,16 @@
+package com.shoes101.service.impl;
+
+import com.shoes101.service.GoodsMService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class GoodsMServiceImpl implements GoodsMService {
+
+    @Override
+    public String addShoesInformationAjax() {
+
+
+        return null;
+    }
+}
