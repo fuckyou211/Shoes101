f25239522c4d6c8729f4b55c883ae0216f54568f
Tbestf <1109102506@qq.com>
2018-09-13 15:29:19 +0800

    Catalo AJAX访问列表

--- src/main/java/com/shoes101/controller/GoodsMController.java
+++ src/main/java/com/shoes101/controller/GoodsMController.java
@@ -6,6 +6,7 @@ import org.hibernate.validator.constraints.EAN;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -33,6 +34,15 @@ public class GoodsMController {
        return  goodsMService.addShoesInformationAjax();
     //    return  JSONObject.toJSONString("111");
     }
+
+    @RequestMapping("/shoesCatalogAjax")
+    @ResponseBody
+    public String shoesCatalogAjax(@RequestParam(name="parentId",required=false) Integer parentId){
+
+        return  goodsMService.shoesCatalogAjax(parentId);
+        //    return  JSONObject.toJSONString("111");
+    }
+
 
 
 }
