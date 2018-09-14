6a49555e3b755c101165db880f123b439f3ebe65
Tbestf <1109102506@qq.com>
2018-09-14 10:10:50 +0800

    属性值获取

--- src/main/java/com/shoes101/controller/GoodsMController.java
+++ src/main/java/com/shoes101/controller/GoodsMController.java
@@ -3,11 +3,16 @@ package com.shoes101.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.shoes101.service.GoodsMService;
 import org.hibernate.validator.constraints.EAN;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 添加商品管理
@@ -16,6 +21,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/goodsM")
 public class GoodsMController {
+
+    private  final Logger logger= LoggerFactory.getLogger(GoodsMController.class) ;
 
     @Autowired
     GoodsMService goodsMService;
@@ -30,7 +37,7 @@ public class GoodsMController {
     @RequestMapping("/addShoesAjax")
     @ResponseBody
     public String addShoesInformationAjax(){
-
+        logger.info("addShoesInformationAjax");
        return  goodsMService.addShoesInformationAjax();
     //    return  JSONObject.toJSONString("111");
     }
@@ -41,6 +48,21 @@ public class GoodsMController {
 
         return  goodsMService.shoesCatalogAjax(parentId);
         //    return  JSONObject.toJSONString("111");
+    }
+
+
+    //多文件上传
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    @ResponseBody
+    public String upload(HttpServletRequest request) {
+
+        return goodsMService.upload(request);
+    }
+
+    @RequestMapping("/shoesPropertyAjax")
+    @ResponseBody
+    public String shoesPropertyAjax(@RequestParam(name="propertyId",required=false) Integer propertyId){
+        return goodsMService.shoesPropertyAjax(propertyId);
     }
 
 
