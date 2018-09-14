6ac8924bf06b61bc787981dae8596f934cdc68c4
Tbestf <1109102506@qq.com>
2018-09-14 10:12:06 +0800

    属性值获取和文件上传功能

--- src/main/java/com/shoes101/service/GoodsMService.java
+++ src/main/java/com/shoes101/service/GoodsMService.java
@@ -1,9 +1,15 @@
 package com.shoes101.service;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface GoodsMService {
 
     public String addShoesInformationAjax();
 
     public String shoesCatalogAjax(Integer parentId);
+
+    public String upload(HttpServletRequest request);
+
+    public String shoesPropertyAjax(Integer propertyId);
 
 }
