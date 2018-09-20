b29a02bbc99cd4b31053e6ac7bbe189759ebd9ad
DlSco <734808034@qq.com>
2018-09-20 15:10:40 +0800

    添加异常处理类

---
+++ src/main/java/com/shoes101/exception/GlobalException.java
@@ -0,0 +1,21 @@
+package com.shoes101.exception;
+
+
+import com.shoes101.result.CodeMsg;
+
+public class GlobalException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	private CodeMsg cm;
+	
+	public GlobalException(CodeMsg cm) {
+		super(cm.toString());
+		this.cm = cm;
+	}
+
+	public CodeMsg getCm() {
+		return cm;
+	}
+
+}
