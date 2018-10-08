952d11e598771c07e38bbb41fd99da3884d09f61
DlSco <734808034@qq.com>
2018-10-08 08:18:28 +0800

    修正部分bug

--- src/main/java/com/shoes101/controller/NavBarController.java
+++ src/main/java/com/shoes101/controller/NavBarController.java
@@ -1,21 +1,36 @@
 package com.shoes101.controller;
 
+import com.shoes101.pojo.Shoescatalog;
 import com.shoes101.result.Result;
 import com.shoes101.service.NavBarService;
 import com.shoes101.vo.CatalogInfoVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class NavBarController {
 
     @Autowired
     private NavBarService navBarService;
-    @RequestMapping("/getCatalogInfo")
+
+    @RequestMapping("/getCatalogInfo/{level}")
     @ResponseBody
-    public Result<CatalogInfoVo> getCatalogInfo(){
-        return Result.success( navBarService.getCatalogInfo());
+    public Result<Map<String,List<CatalogInfoVo>>> getNavBarInfo( @PathVariable("level") Integer level,String catalogName[],Integer parentId){
+
+        Map<String,List<CatalogInfoVo>> map = new HashMap<String, List<CatalogInfoVo>>();
+        for(int i = 0;i<catalogName.length;i++){
+            Shoescatalog shoescatalog = navBarService.selectByNameAndParentId(parentId,catalogName[i]);
+            List<CatalogInfoVo> list = navBarService.getCatalogInfo(shoescatalog,level);
+            map.put(catalogName[i],list);
+        }
+        return  Result.success( map);
     }
+
 }
--- src/main/java/com/shoes101/service/NavBarService.java
+++ src/main/java/com/shoes101/service/NavBarService.java
@@ -1,9 +1,18 @@
 package com.shoes101.service;
 
 
+import com.shoes101.pojo.Shoescatalog;
 import com.shoes101.vo.CatalogInfoVo;
+
+import java.util.List;
+import java.util.Map;
 
 public interface NavBarService {
 
-    public CatalogInfoVo getCatalogInfo();
+    //某节点下数level层内的封装数据信息
+    public List<CatalogInfoVo> getCatalogInfo(Shoescatalog shoescatalog, Integer level);
+
+    //根据名字和parentid找到节点
+    public Shoescatalog selectByNameAndParentId(Integer parentId,String catalogName);
+
 }
