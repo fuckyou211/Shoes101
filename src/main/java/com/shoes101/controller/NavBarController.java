03361cbe510fe2d8c1ff5de62f6e372b7a3d6942
DlSco <734808034@qq.com>
2018-10-08 15:04:59 +0800

    修改NavBarController

--- src/main/java/com/shoes101/controller/NavBarController.java
+++ src/main/java/com/shoes101/controller/NavBarController.java
@@ -1,5 +1,7 @@
 package com.shoes101.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.shoes101.pojo.Shoescatalog;
 import com.shoes101.result.Result;
 import com.shoes101.service.NavBarService;
@@ -7,6 +9,7 @@ import com.shoes101.vo.CatalogInfoVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,15 +23,17 @@ public class NavBarController {
     @Autowired
     private NavBarService navBarService;
 
-    @RequestMapping("/getCatalogInfo/{level}")
     @ResponseBody
-    public Result<Map<String,List<CatalogInfoVo>>> getNavBarInfo( @PathVariable("level") Integer level,String catalogName[],Integer parentId){
+    @RequestMapping("/getCatalogInfo/{level}")
+    public Result<Map<String,List<CatalogInfoVo>>> getNavBarInfo(@PathVariable("level") Integer level, String catalogNameInfo, Integer parentId){
 
+        JSONArray catalogName = JSON.parseArray(catalogNameInfo);
+        System.out.println(catalogName);
         Map<String,List<CatalogInfoVo>> map = new HashMap<String, List<CatalogInfoVo>>();
-        for(int i = 0;i<catalogName.length;i++){
-            Shoescatalog shoescatalog = navBarService.selectByNameAndParentId(parentId,catalogName[i]);
+        for(int i = 0;i<catalogName.size();i++){
+            Shoescatalog shoescatalog = navBarService.selectByNameAndParentId(parentId, (String) catalogName.get(i));
             List<CatalogInfoVo> list = navBarService.getCatalogInfo(shoescatalog,level);
-            map.put(catalogName[i],list);
+            map.put((String) catalogName.get(i),list);
         }
         return  Result.success( map);
     }
