c41c54bb5b7ccc00c9d9e287a34666a8ca81c787
fuckyou211 <527644254@qq.com>
2018-10-08 18:49:08 +0800

    修改商品详情

---
+++ src/main/java/com/shoes101/vo/ColAndPicVo.java
@@ -0,0 +1,35 @@
+package com.shoes101.vo;
+
+/**
+ * 颜色和对应图片
+ */
+public class ColAndPicVo {
+
+    private int colorid;
+    private String color;
+    private String colorpic;
+
+    public int getColorid() {
+        return colorid;
+    }
+
+    public void setColorid(int colorid) {
+        this.colorid = colorid;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColorpic() {
+        return colorpic;
+    }
+
+    public void setColorpic(String colorpic) {
+        this.colorpic = colorpic;
+    }
+}
