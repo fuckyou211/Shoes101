5aa12d51d610d69a3792574fca52ef07d559152a
DlSco <734808034@qq.com>
2018-10-09 15:47:50 +0800

    修改了

--- src/main/java/com/shoes101/controller/FrontStage/ShoesHeaderController.java
+++ src/main/java/com/shoes101/controller/FrontStage/ShoesHeaderController.java
@@ -5,13 +5,12 @@ import com.alibaba.fastjson.JSONArray;
 import com.shoes101.pojo.Propertyvalue;
 import com.shoes101.pojo.Shoescatalog;
 import com.shoes101.result.Result;
-import com.shoes101.service.NavBarService;
+import com.shoes101.service.ShoesHeaderService;
 import com.shoes101.service.PropertyService;
 import com.shoes101.vo.CatalogInfoVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -21,10 +20,10 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/header")
-public class NavBarController {
+public class ShoesHeaderController {
 
     @Autowired
-    private NavBarService navBarService;
+    private ShoesHeaderService shoesHeaderService;
     @Autowired
     private PropertyService propertyService;
 
@@ -36,13 +35,12 @@ public class NavBarController {
         System.out.println(catalogNameInfo);
         Map<String,List<CatalogInfoVo>> map = new HashMap<String, List<CatalogInfoVo>>();
         for(int i = 0;i<catalogName.size();i++){
-            Shoescatalog shoescatalog = navBarService.selectByNameAndParentId(parentId, (String) catalogName.get(i));
-            List<CatalogInfoVo> list = navBarService.getCatalogInfo(shoescatalog,level);
+            Shoescatalog shoescatalog = shoesHeaderService.selectByNameAndParentId(parentId, (String) catalogName.get(i));
+            List<CatalogInfoVo> list = shoesHeaderService.getCatalogInfo(shoescatalog,level);
             map.put((String) catalogName.get(i),list);
         }
         return  Result.success( map);
     }
-
     @RequestMapping("/getBrandInfo")
     @ResponseBody
     public Result<List<Propertyvalue>> getBrandInfo(Integer propertyId){
@@ -50,9 +48,21 @@ public class NavBarController {
         return Result.success(propertyService.getProperty(propertyId));
     }
     @RequestMapping("/toShoes-header")
-    public String toShoesHeader(){
+    public String toShoesHeader(HashMap<String,Object> hm){
+        //初始化CatalogInfo
+        hm.put("navBarInfo",JSON.toJSONString(Result.success(shoesHeaderService.initCatalogInfo())));
+        System.out.println(shoesHeaderService.initCatalogInfo());
+        //初始化BrandInfo
+        String propertyName = "品牌";
+        Integer propertyId = propertyService.getPropertyIdByPropertyName(propertyName);
+        System.out.println(propertyService.getProperty(propertyId));
+        if(propertyId!=null)
+            hm.put("brandInfo",JSON.toJSONString(Result.success(propertyService.getProperty(propertyId))));
         return "/front/shoes-header";
     }
-
-
+    @RequestMapping("/handleClickNavBarCatalog")
+    @ResponseBody
+    public String handleClickNavBarCatalog(Integer catalogId){
+        return "";
+    }
 }
