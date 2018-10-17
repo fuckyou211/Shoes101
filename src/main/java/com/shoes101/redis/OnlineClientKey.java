08985d30fdc053bf54767f68433eb36ff1a17362
DlSco <734808034@qq.com>
2018-10-17 15:19:41 +0800

    漏了文件

---
+++ src/main/java/com/shoes101/redis/OnlineClientKey.java
@@ -0,0 +1,20 @@
+package com.shoes101.redis;
+
+public class OnlineClientKey extends BasePrefix{
+
+    public OnlineClientKey(String prefix) {
+        super(prefix);
+    }
+
+    public static final int TOKEN_EXPIRE = 3600*24 * 2;
+    /**
+     *
+     * @param expireSeconds  有效时间
+     * @param prefix  key前缀
+     */
+    public OnlineClientKey(int expireSeconds, String prefix) {
+        super(expireSeconds, prefix);
+    }
+    public static OnlineClientKey onlineAdmin = new OnlineClientKey( "oa");
+    public static OnlineClientKey onlineUser = new OnlineClientKey( "oc");
+}
