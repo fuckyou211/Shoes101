f9d30b7c08ce1c2d0332f5cb4882e7c6ee26bc03
DlSco <734808034@qq.com>
2018-10-21 13:35:24 +0800

    解决运行报错

--- src/main/java/com/shoes101/controller/FrontStage/SearchController.java
+++ src/main/java/com/shoes101/controller/FrontStage/SearchController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SearchController {
     //针对首页搜索框
-    @RequestMapping("/ShoesShop/shoes-list")
+   /* @RequestMapping("/ShoesShop/shoes-list")
     public String searchForHomePage(String value){
         return null;
-    }
+    }*/
 }
